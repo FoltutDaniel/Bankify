aaa51be1edc8057ee64b726fbcdaf9c71b88da1e
Foltut Daniel <danfoltut@gmail.com>
2021-04-21 21:10:41 +0300

    first commit

---
+++ Bankify/Form_Login.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Bankify
+{
+    partial class Form_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(56, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USERNAME:";
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(195, 142);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(186, 20);
+            this.textBox_username.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(56, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PASSWORD:";
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(195, 212);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
+            this.textBox_password.Size = new System.Drawing.Size(186, 20);
+            this.textBox_password.TabIndex = 4;
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(195, 285);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(106, 48);
+            this.button_login.TabIndex = 5;
+            this.button_login.Text = "LOGIN";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // Form_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 393);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.label1);
+            this.Name = "Form_login";
+            this.Text = "Login_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_login;
+    }
+}
+
---
+++ Bankify/Form_Login.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bankify
+{
+    public partial class Form_login : Form
+    {
+        public Form_login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string username = textBox_username.Text;
+            string password = textBox_password.Text;
+            int login_id=0;
+            string account_type = "";
+            using (var db=new Bank_dbEntities())
+            {
+                try
+                {
+                    var query = (from b in db.LoginAccount
+                                 where b.login_username == username && b.login_password == password
+                                 select b).First();
+
+                    login_id = query.login_id;
+
+                    var query1 = (from b in db.LoginAccount
+                                  where b.login_id == login_id
+                                  select b).First();
+
+                    account_type = query1.account_type;
+
+                    if (account_type == "client")
+                    {
+                        Form_Client form_Client = new Form_Client();
+                        form_Client.Show();
+                    }
+                    else
+                    {
+                        if (account_type == "admin")
+                        {
+                            Form_Admin form_Admin = new Form_Admin();
+                            form_Admin.Show();
+                        }
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    try
+                    {
+                        var query= (from b in db.LoginAccount
+                                    where b.login_username == username 
+                                    select b).First();
+                        try
+                        {
+                            var query1 = (from b in db.LoginAccount
+                                         where b.login_password == password
+                                         select b).First();
+                        }
+                        catch(InvalidOperationException ex1)
+                        {
+                            MessageBox.Show("Parola este gresita");
+                        }
+                    }
+                    catch(InvalidOperationException ex2)
+                    {
+                        MessageBox.Show("Username-ul nu exista");
+                    }
+                }
+            }
+        }
+    }
+}
